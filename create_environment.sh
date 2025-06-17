c8efbacfe4d357b50335475e4e3db9478034d8c8
cashme8 <kabandagislain440@gmail.com>
2025-06-17 10:32:49 +0000

    the folder is created, the files are create but i can not put data in themgit add . echo command can'not send data as i want, i need some this smaller to work with

--- create_environment.sh
+++ create_environment.sh
@@ -1,16 +1,45 @@
 #!bin/bash
 
 # first we ask user for the name
- read -p "Enter your name" name
+ read -p "Enter your name:  " name
 
 # Create the folder or directory of files submission_remider_{user's name}
 
-mkdir "submission_reminder_$name"
- 
-# create PATH
-cd "submission_reminder_$name" && mkdir "app" && touch "app/reminder.sh"
-cd "submission_reminder_$name" && mkdir "modules" && touch "modules/functions.sh"
-cd "submission_reminder_$name" && mkdir "assets" && touch "assets submissions.txt"
-cd "submission_reminder_$name" && mkdir "config" && touch "config/config.env"
-cd "submission_reminder_$name" && touch "startup.sh"
+folder="submission_reminder_$name"
 
+mkdir -p "$folder"
+
+# create the startup.sh file
+
+touch "$folder/startup.sh"
+echo -e "#!/bin/bash/n1 ./scripts/reminder.sh"
+
+# Directory = file mapping
+
+declare -A file_map=(
+	["app"]="reminder.sh"
+	["modules"]="functions.sh"
+	["assets"]="submissions.txt"
+	["config"]="config.env"
+)
+declare -A files_content=(
+["reminder.sh"]="#!/bin/bash
+source ./scripts/functions.sh
+source ./config/config.env
+
+echo "Reminder: These students have not submitted the assignment: \$ASSIGNMENT"
+grep -v "submitted" submissions.txt | cut -d',' -f1")
+
+
+# create each directory and its unique file with a loop
+
+for dir in "${!file_map[@]}"; do
+	path="$folder/$dir"
+	mkdir -p "$path"
+	touch "$path/${file_map[$dir]}"
+
+	mkdir -p "$dir_path"
+	echo -e "${}"
+done
+
+# found a way to jump to the next line
